6e6270ee015325f4abaedc3052b8a3c989b42571
sumitk68 <imsumitk1@gmail.com>
2019-01-31 20:13:11 -0500

    Making objects in Main java

--- src/com/lambton/magicalcardgame/MagicalCardGameModel.java
+++ src/com/lambton/magicalcardgame/MagicalCardGameModel.java
@@ -1,33 +1,60 @@
 package com.lambton.magicalcardgame;
 
-import java.util.Arrays;
 
-public class MagicalCardGameModel  {
-
-String[][] magiccard={   {"1","6","7"},
-        {"j","k","r"},
-        {"4","5","8"}
-};
+public class MagicalCardGameModel {
     String firstShuffleColPos;
     String secShuffleColPos;
-    String[][]cardList=null;
-    String[][]firstShuffle=null;
-    String[][]secShuffle=null;
+    String [][]cardList=null;
+    String [][]firstShuffle=null;
+    String [][]secShuffle=null;
     String guessedCard;
 
-
-    public String[][] getFirstShuffleResult(MagicalCardGameModel magicalCardGameModel) {
-        return new String[0][];
+    public String getFirstShuffleColPos() {
+        return firstShuffleColPos;
     }
 
-
-    public String[][]
-    getSecShuffleResult(MagicalCardGameModel magicalCardGameModel) {
-        return new String[0][];
+    public void setFirstShuffleColPos(String firstShuffleColPos) {
+        this.firstShuffleColPos = firstShuffleColPos;
     }
 
+    public String getSecShuffleColPos() {
+        return secShuffleColPos;
+    }
 
-    public String getFinalResult(MagicalCardGameModel magicalCardGameModel) {
-        return null;
+    public void setSecShuffleColPos(String secShuffleColPos) {
+        this.secShuffleColPos = secShuffleColPos;
+    }
+
+    public String[][] getCardList() {
+        return cardList;
+    }
+
+    public void setCardList(String[][] cardList) {
+        this.cardList = cardList;
+    }
+
+    public String[][] getFirstShuffle() {
+        return firstShuffle;
+    }
+
+    public void setFirstShuffle(String[][] firstShuffle) {
+        this.firstShuffle = firstShuffle;
+    }
+
+    public String[][] getSecShuffle() {
+        return secShuffle;
+    }
+
+    public void setSecShuffle(String[][] secShuffle) {
+        this.secShuffle = secShuffle;
+    }
+
+    public String getGuessedCard() {
+        return guessedCard;
+    }
+
+    public void setGuessedCard(String guessedCard) {
+        this.guessedCard = guessedCard;
     }
 }
+
--- src/com/lambton/magicalcardgame/MainMagic.java
+++ src/com/lambton/magicalcardgame/MainMagic.java
@@ -5,7 +5,11 @@ public class MainMagic {
     public static void main(String[] arg){
 
 
+        MagicalCardGameModel magicalCardGameModel= new MagicalCardGameModel();
+        MagicalCardGameCalculator magicalCardGameCalculator= new MagicalCardGameCalculator();
+        String[][] Matrix={{ "9", "2", "6" }, { "j", "l", "q" }, { "7", "8", "5" }};
         System.out.print("Please select anyone no. from card matrix");
+
 
 
             }
