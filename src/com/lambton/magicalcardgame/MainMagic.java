815e94698620277d8f7b20f948b183908245e40d
sumitk68 <imsumitk1@gmail.com>
2019-01-31 20:19:53 -0500

    insert Scanner in Main

--- src/com/lambton/magicalcardgame/MainMagic.java
+++ src/com/lambton/magicalcardgame/MainMagic.java
@@ -1,16 +1,30 @@
 package com.lambton.magicalcardgame;
 
+import java.util.Scanner;
+
 public class MainMagic {
 
-    public static void main(String[] arg){
+    public static void main(String[] arg) {
 
 
-        MagicalCardGameModel magicalCardGameModel= new MagicalCardGameModel();
-        MagicalCardGameCalculator magicalCardGameCalculator= new MagicalCardGameCalculator();
-        String[][] Matrix={{ "9", "2", "6" }, { "j", "l", "q" }, { "7", "8", "5" }};
+        MagicalCardGameModel magicalCardGameModel = new MagicalCardGameModel();
+        MagicalCardGameCalculator magicalCardGameCalculator = new MagicalCardGameCalculator();
+        String[][] matrix = {{"9", "2", "6"}, {"j", "l", "q"}, {"7", "8", "5"}};
+        magicalCardGameModel.setCardList(matrix);
+        showMatrix(magicalCardGameModel.getCardList());
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the column Number which contains your card");
         System.out.print("Please select anyone no. from card matrix");
 
 
+    }
 
+    private static void showMatrix(String[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
             }
+            System.out.println();
+        }
+    }
 }
